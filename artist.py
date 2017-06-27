64fadea1fb6aa4f93301629f1a4ec395cfc85bef
Maanav Khaitan <astromanav@gmail.com>
2017-06-27 13:34:33 -0400

    Update artist.py

--- artist.py
+++ artist.py
@@ -5,6 +5,8 @@ import numpy as np
 from scipy.optimize import fmin_l_bfgs_b
 from scipy.misc import imsave
 
+import time
+
 from PIL import Image
 
 from keras import backend
@@ -18,11 +20,11 @@ width = 512
 
 
 # Open and resize original image
-original_image = Image.open('/Users/maanavkhaitan/Downloads/hugo.jpg')
+original_image = Image.open('/Users/maanavkhaitan/Downloads/square.jpg')
 original_image = original_image.resize((height, width))
 
 # Open and resize style image
-style_image = Image.open('/Users/maanavkhaitan/Downloads/wave.jpg')
+style_image = Image.open('/Users/maanavkhaitan/Downloads/marilyn.jpg')
 style_image = style_image.resize((height, width))
 
 
@@ -50,11 +52,11 @@ style_array = style_array[:, :, :, ::-1]
 original_image = backend.variable(original_array)
 style_image = backend.variable(style_array)
 
-# Create placeholder for output image with same dimensions
-output_image = backend.placeholder((1, height, width, 3))
+# Create placeholder for combined image with same dimensions
+combined_image = backend.placeholder((1, height, width, 3))
 
 # Create tensor with all three images: original, style, and output
-input_tensor = backend.concatenate([original_image, style_image, output_image], axis=0)
+input_tensor = backend.concatenate([original_image, style_image, combined_image], axis=0)
 
 
 # Initialize VGG16 model in Keras and set default image classification weights
@@ -64,8 +66,139 @@ model = VGG16(input_tensor=input_tensor, weights='imagenet', include_top=False)
 layers = dict([(layer.name, layer.output) for layer in model.layers])
 
 
+# Set arbitrary weights for content, style, and total variation loss
+content_weight = 0.025
+style_weight = 5.0
+total_variation_weight = 1.0
+
+# Initialize complete loss to 0
+complete_loss = backend.variable(0.)
 
 
+def content_loss(original, combined):
+	# Calculates Euclidean distance between combined and original image
+    return backend.sum(backend.square(combined - original))
+
+# Choose a hidden layer in VGG16 network and extract features for original and combined images in this layer
+layer_features = layers['block2_conv2']
+original_image_features = layer_features[0, :, :, :]
+combined_image_features = layer_features[2, :, :, :]
+
+# Add weighted content loss to complete loss
+complete_loss += content_weight * content_loss(original_image_features,combined_image_features)
 
 
+def gram_matrix(x):
+	# Calculates Gram Matrix
 
+	# Reshape feature spaces so gram matrix can be calculated
+    new_feature_space = backend.batch_flatten(backend.permute_dimensions(x, (2, 0, 1)))
+
+    # Take outer product of reshaped feature space and its transpose
+    g_matrix = backend.dot(new_feature_space, backend.transpose(new_feature_space))
+    return g_matrix
+
+def style_loss(style_image, combined_image):
+# Calculates loss of style between combined image and style image
+
+	# Calculate Gram Matrix for style and combined image
+    S = gram_matrix(style_image)
+    C = gram_matrix(combined_image)
+
+    # Calculate Frobenius Norm of difference of style and combined Gram Matrices
+    channels = 3
+    size = height * width
+    return backend.sum(backend.square(S - C)) / (4. * (channels ** 2) * (size ** 2))
+
+# Create list of hidden layers that we can extract features about style and combined images
+feature_layers = ['block1_conv2', 'block2_conv2', 'block3_conv3', 'block4_conv3', 'block5_conv3']
+
+# Loop over every layer in feature_layers
+for layer in feature_layers:
+
+	# Extract features of all images in layer
+    layer_features = layers[layer]
+
+    # Extract features of style and combined images
+    style_features = layer_features[1, :, :, :]
+    combined_features = layer_features[2, :, :, :]
+
+    # Calculate style loss of style and combined images, and add weighted style loss to complete loss
+    sl = style_loss(style_features, combined_features)
+    complete_loss += (style_weight / len(feature_layers)) * sl
+
+
+def total_variation_loss(x):
+# Calculates total variation loss; a regularization term to eliminate noise in combined image
+    a = backend.square(x[:, :height-1, :width-1, :] - x[:, 1:, :width-1, :])
+    b = backend.square(x[:, :height-1, :width-1, :] - x[:, :height-1, 1:, :])
+    return backend.sum(backend.pow(a + b, 1.25))
+
+# Add weighted total variation loss to complete loss
+complete_loss += total_variation_weight * total_variation_loss(combined_image)
+
+
+# Calculate gradients for complete loss relative to combined image
+gradients = backend.gradients(complete_loss, combined_image)
+
+outputs = [complete_loss]
+outputs += gradients
+f_outputs = backend.function([combined_image], outputs)
+
+def find_loss_and_gradients(x):
+    x = x.reshape((1, height, width, 3))
+    outs = f_outputs([x])
+    loss_value = outs[0]
+    gradient_values = outs[1].flatten().astype('float64')
+    return loss_value, gradient_values
+
+
+class Evaluator(object):
+# Returns loss and gradients in two separate functions
+
+    def __init__(self):
+    	# Initialize loss and gradient values as None
+        self.loss_value = None
+        self.gradient_values = None
+
+    def loss(self, x):
+    	# Returns loss value is loss value has not already been computed
+        assert self.loss_value is None
+        loss_value, gradient_values = find_loss_and_gradients(x)
+        self.loss_value = loss_value
+        self.gradient_values = gradient_values
+        return self.loss_value
+
+    def grads(self, x):
+    	# Returns gradient value if loss value has been computed
+        assert self.loss_value is not None
+        gradient_values = np.copy(self.gradient_values)
+        self.loss_value = None
+        self.gradient_values = None
+        return gradient_values
+
+evaluator = Evaluator()
+
+
+# Create random initial guesses for optimization function
+x = np.random.uniform(0, 255, (1, height, width, 3)) - 128.
+
+# Set number of iterations of optimization
+iterations = 10
+
+print 'reached till here'
+
+for i in range(0,iterations):
+    x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x.flatten(), fprime=evaluator.grads, maxfun=20)
+    print i
+
+x = x.reshape((height, width, 3))
+x = x[:, :, ::-1]
+x[:, :, 0] += 103.939
+x[:, :, 1] += 116.779
+x[:, :, 2] += 123.68
+x = np.clip(x, 0, 255).astype('uint8')
+
+output_image = Image.fromarray(x)
+output_image.show()
+output_image.save('resultpic.bmp')
